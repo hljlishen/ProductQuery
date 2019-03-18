10c889d7942050c4ee4077159f3b9431a4014a69
admin <admin@DESKTOP-OSRP7ML>
2019-03-18 16:28:25 +0800

    3-18 新建

---
+++ ProductQuery/马华/entity/DelayTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductQuery.马华.entity
+{
+    public class DelayTime
+    {
+        public int dtId { get; set; }
+        public int mainId { get; set; }
+        public double 温度条件 { get; set; }
+        public double 延期时间下限 { get; set; }
+        public double 延期时间上限 { get; set; }
+        public string 延期时间单位 { get; set; }
+        public double 延期时间值 { get; set; }
+        public double 延期时间值误差 { get; set; }
+        public double 延期时间值误差上限 { get; set; }
+        public double 延期时间值误差下限 { get; set; }
+        public string 延期时间备注 { get; set; }
+    }
+}
---
+++ ProductQuery/马华/entity/InterfaceInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductQuery.马华.entity
+{
+    public class InterfaceInformation
+    {
+        public int interfaceId { get; set; }
+        public int mainId { get; set; }
+        public double 螺纹 { get; set; }
+        public double 螺距 { get; set; }
+        public string 公差 { get; set; }
+        public double 长度 { get; set; }
+    }
+}
