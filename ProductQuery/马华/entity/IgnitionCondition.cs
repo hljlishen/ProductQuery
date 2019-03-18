1f2c27beea94904845ac4ae759f139908173c271
admin <admin@DESKTOP-OSRP7ML>
2019-03-18 18:11:34 +0800

    3-18

--- ProductQuery/马华/entity/IgnitionCondition.cs
+++ ProductQuery/马华/entity/IgnitionCondition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProductQuery.马华.entity
+namespace ProductQuery.entity
 {
     public class IgnitionCondition
     {
