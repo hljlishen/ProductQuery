ae4b7d1239d6d7a0c629ba30e6e9953fb9e812b6
lalalayt <yanting1126@hotmail.com>
2019-03-19 09:09:27 +0800

    管理员主页面

--- ProductQuery/Models/燕廷/ProductModel.cs
+++ ProductQuery/Models/燕廷/ProductModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProductQuery.燕廷
+namespace ProductQuery.Models
 {
     public class ProductModel
     {
---
+++ ProductQuery/Models/燕廷/产品示意图.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductQuery.Models
+{
+    public class 产品示意图
+    {
+    }
+}
