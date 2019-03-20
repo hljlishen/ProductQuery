867dc3babfc697b06a9b155e05e060f85e82b467
lalalayt <yanting1126@hotmail.com>
2019-03-20 08:37:31 +0800

    登陆

---
+++ ProductQuery/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductQuery.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult AdministratorPage()
+        {
+            return View();
+        }
+
+        public ActionResult AdminLongin()
+        {
+            return View();
+        }
+
+        public ActionResult AdminNavigation()
+        {
+            return View();
+        }
+    }
+}
