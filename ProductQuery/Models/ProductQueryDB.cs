09453ed781c626167e042f2e4ae719df8b7265f0
lalalayt <yanting1126@hotmail.com>
2019-03-27 15:23:20 +0800

    实体

--- ProductQuery/Models/ProductQueryDB.cs
+++ ProductQuery/Models/ProductQueryDB.cs
@@ -9,5 +9,15 @@ namespace ProductQuery.Models
     public class ProductQueryDB:DbContext
     {
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Ignition> Ignition { get; set; }
+        public DbSet<Picture> Picture { get; set; }
+        public DbSet<CableDiameter> CableDiameter { get; set; }
+        public DbSet<Conventional> Conventional { get; set; }
+        public DbSet<DcResistance> DcResistance { get; set; }
+        //public DbSet<DelayTime1> DelayTime1 { get; set; }
+        public DbSet<IgnitionCondition> IgnitionCondition { get; set; }
+        public DbSet<InterfaceInformation> InterfaceInformation { get; set; }
+        public DbSet<SpeedDetonation> SpeedDetonation { get; set; }
     }
 }
